e18189f2c59e9a8b49f234163ee15a6516384a29
Robert Goldmann <deadlocker@gmx.de>
2017-01-26 19:25:14 +0100

    started working on spark server

---
+++ src/de/deadlocker8/budgetmasterserver/main/Settings.java
@@ -0,0 +1,22 @@
+package de.deadlocker8.budgetmasterserver.main;
+
+public class Settings
+{
+	private int port;	
+	private String secret;
+	
+	public Settings()
+	{
+		
+	}
+
+	public int getPort()
+	{
+		return port;
+	}
+
+	public String getSecret()
+	{
+		return secret;
+	}
+}
--- src/de/deadlocker8/budgetmasterserver/server/SparkServer.java
+++ src/de/deadlocker8/budgetmasterserver/server/SparkServer.java
@@ -1,6 +1,81 @@
 package de.deadlocker8.budgetmasterserver.server;
 
+import static spark.Spark.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import de.deadlocker8.budgetmaster.logic.Category;
+import de.deadlocker8.budgetmasterserver.main.DatabaseHandler;
+import de.deadlocker8.budgetmasterserver.main.Settings;
+import spark.Spark;
+import spark.route.RouteOverview;
+
 public class SparkServer
 {
+	private static Gson gson;
+	
+	public static void main(String[] args) throws URISyntaxException
+	{		
+		gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		if (!Files.exists(Paths.get("settings.properties")))
+		{
+			try
+			{
+				Files.copy(SparkServer.class.getClassLoader().getResourceAsStream("de/deadlocker8/budgetmasterserver/resources/settings.properties"), Paths.get("settings.properties"));
+			}
+			catch(IOException e)
+			{
+				//ERRORHANDLING
+				e.printStackTrace();
+			}		
+		}
+		
+		String settingsJSON;
+		Settings settings;
+		try
+		{
+			settingsJSON = new String(Files.readAllBytes(Paths.get("settings.properties")));				
+			settings = gson.fromJson(settingsJSON, Settings.class);	
+		}
+		catch(IOException e)
+		{
+			//ERRORHANDLING
+			e.printStackTrace();
+			return;
+		}
+		
+		port(settings.getPort());
+		//TODO HTTPS
+		//secure("", "", null, null);	
+		
+		before((request, response) -> {
+			
+			String clientSecret = request.queryMap("secret").value();
+		
+			if(clientSecret == null || !clientSecret.equals(settings.getSecret()))			
+			{
+				halt(401, "Unauthorized");
+			}
+		});
 
+		get("/hello", (req, res) -> {
+
+			DatabaseHandler handler = new DatabaseHandler();			
+			ArrayList<Category> categories = handler.getCategories();
+			System.out.println(req.queryMap("id").doubleValue());
+
+			return gson.toJson(categories);
+		});
+
+		// DEBUG
+		RouteOverview.enableRouteOverview();
+	}
 }
