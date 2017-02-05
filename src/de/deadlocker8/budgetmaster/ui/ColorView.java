23db68de4257b0c0692e97a9cb31f8a1c3b0e70d
Robert Goldmann <deadlocker@gmx.de>
2017-02-05 21:12:03 +0100

    Fixed #26, Fixed #25 with custom colorpicker (colorPick from Tools)

--- src/de/deadlocker8/budgetmaster/ui/ColorView.java
+++ src/de/deadlocker8/budgetmaster/ui/ColorView.java
@@ -3,20 +3,30 @@ package de.deadlocker8.budgetmaster.ui;
 import java.util.ArrayList;
 import java.util.function.Consumer;
 
+import de.deadlocker8.budgetmaster.ui.colorPick.ColorPickController;
 import fontAwesome.FontIcon;
 import fontAwesome.FontIconType;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.control.ColorPicker;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import logger.LogLevel;
+import logger.Logger;
 import tools.ConvertTo;
 
 public class ColorView extends GridPane
 {
 	private Node lastSelectedNode;
+	private Color colorPickerColor;
+	private Button buttonColorPicker;
+	private FontIcon icon;
 	
 	public ColorView(Color startColor, ArrayList<Color> colors, NewCategoryController controller, Consumer<Color> finish)
 	{		
@@ -32,6 +42,8 @@ public class ColorView extends GridPane
 		setVgap(5);
 		setHgap(5);
 		setPadding(new Insets(5));
+		
+		colorPickerColor = Color.WHITE;
 
 		int index = 0;
 		for(int y = 0; y < iSize; y++)
@@ -43,41 +55,55 @@ public class ColorView extends GridPane
 					Color color = colors.get(index++);
 
 					if(color == Color.TRANSPARENT)
-					{	
-						StackPane stackPane = new StackPane();				
-						
-						ColorPicker picker = new ColorPicker();	
-						picker.setPrefHeight(40);
-						picker.setPrefWidth(40);
-						picker.setMaxWidth(40);
-						updateColorPickerCSS(picker, false, Color.TRANSPARENT);
-						stackPane.getChildren().add(picker);					
-						
+					{						
+						buttonColorPicker = new Button();	
+						buttonColorPicker.setPrefHeight(40);
+						buttonColorPicker.setPrefWidth(40);
+												
 						if(!colors.contains(startColor))
-						{
-							picker.setValue(startColor);
-							updateColorPickerCSS(picker, true, startColor);
-						}
+						{								
+							colorPickerColor = startColor;
+							updateColorPickerCSS(buttonColorPicker, true, startColor);
+						}	
 						
-						FontIcon icon = new FontIcon(FontIconType.PLUS);
+						icon = new FontIcon(FontIconType.PLUS);
 						icon.setSize(20);			
-						icon.setStyle("-fx-text-fill: " + ConvertTo.toRGBHex(ConvertTo.getAppropriateTextColor(picker.getValue())));
-						stackPane.getChildren().add(icon);						
+						icon.setStyle("-fx-text-fill: " + ConvertTo.toRGBHex(ConvertTo.getAppropriateTextColor(colorPickerColor)));						
+						buttonColorPicker.setGraphic(icon);
 
-						picker.valueProperty().addListener((observable, oldValue, newValue)->{
-							updateColorPickerCSS(picker, true, newValue);
-							if(lastSelectedNode instanceof Rectangle)
+						buttonColorPicker.setOnAction((event)->{
+							try
 							{
-								((Rectangle)lastSelectedNode).getStrokeDashArray().clear();
+								FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("de/deadlocker8/budgetmaster/ui/colorPick/ColorPickGUI.fxml"));
+								Parent root = (Parent)loader.load();
+
+								Scene scene = new Scene(root, 500, 225);
+								Stage stage = new Stage();
+								
+								((ColorPickController)loader.getController()).init(stage, (finishColor)->{
+									colorPickerColor = finishColor;
+									updateColorPickerCSS(buttonColorPicker, true, finishColor);
+									if(lastSelectedNode instanceof Rectangle)
+									{
+										((Rectangle)lastSelectedNode).getStrokeDashArray().clear();
+									}
+									lastSelectedNode = buttonColorPicker;
+									icon.setStyle("-fx-text-fill: " + ConvertTo.toRGBHex(ConvertTo.getAppropriateTextColor(finishColor)));
+									finish.accept(finishColor);
+								});
+								
+								stage.setResizable(false);	
+								stage.initModality(Modality.APPLICATION_MODAL);
+								stage.setScene(scene);
+								stage.show();
 							}
-							lastSelectedNode = picker;
-							icon.setStyle("-fx-text-fill: " + ConvertTo.toRGBHex(ConvertTo.getAppropriateTextColor(newValue)));
-							finish.accept(newValue);
-						});						
+							catch(Exception e)
+							{
+								Logger.log(LogLevel.ERROR, Logger.exceptionToString(e));
+							}
+						});
 						
-						icon.setOnMouseClicked((e)-> picker.show());
-						
-						add(stackPane, x, y);
+						add(buttonColorPicker, x, y);
 					}
 					else
 					{
@@ -91,7 +117,8 @@ public class ColorView extends GridPane
 							}
 							else
 							{
-								updateColorPickerCSS(lastSelectedNode, false, ((ColorPicker)lastSelectedNode).getValue());
+								updateColorPickerCSS(buttonColorPicker, false, Color.WHITE);
+								icon.setStyle("-fx-text-fill: " + ConvertTo.toRGBHex(ConvertTo.getAppropriateTextColor(Color.WHITE)));
 							}
 							rectangle.getStrokeDashArray().add(3.0);
 							lastSelectedNode = rectangle;
@@ -104,6 +131,8 @@ public class ColorView extends GridPane
 				}
 			}
 		}
+		
+		updateColorPickerCSS(buttonColorPicker, true, colorPickerColor);
 	}
 
 	private Rectangle createRectangle(Color startColor, Color color)
@@ -122,7 +151,7 @@ public class ColorView extends GridPane
 		return rectangle;
 	}
 	
-	private void updateColorPickerCSS(Node colorPicker, boolean dashed, Color backgroundColor)
+	private void updateColorPickerCSS(Node button, boolean dashed, Color backgroundColor)
 	{
 		String css = "-fx-background-radius: 4; -fx-border-width: 1.8; -fx-border-color: black; -fx-border-radius: 4; -fx-background-color: " + ConvertTo.toRGBHex(backgroundColor) + ";";
 		if(dashed)
@@ -130,6 +159,6 @@ public class ColorView extends GridPane
 			css += " -fx-border-style: dashed;";
 		}
 		
-		colorPicker.setStyle(css);
+		button.setStyle(css);
 	}
 }
