403948e8363e145d8f3daf3111b8bac39fdf77af
Robert Goldmann <deadlocker@gmx.de>
2017-03-28 18:53:05 +0200

    Fixed #57 - disable editing of payment rest

--- src/de/deadlocker8/budgetmaster/ui/PaymentController.java
+++ src/de/deadlocker8/budgetmaster/ui/PaymentController.java
@@ -66,7 +66,11 @@ public class PaymentController implements Refreshable
 						if(event.getClickCount() == 2)
 						{
 							PaymentCell c = (PaymentCell)event.getSource();
-							payment(!c.getItem().isIncome(), true, c.getItem());
+							//don't allow editing of payment "rest"
+							if(c.getItem().getCategoryID() != 2)
+							{
+								payment(!c.getItem().isIncome(), true, c.getItem());
+							}
 						}
 					}
 				});
