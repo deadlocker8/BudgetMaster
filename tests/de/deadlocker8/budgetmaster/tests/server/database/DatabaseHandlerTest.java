39bfabf73463cbf18a6506e153c40ceedfb313e5
Robert Goldmann <deadlocker@gmx.de>
2017-06-14 20:02:49 +0200

    #111 - added tests
    - NormalPayment

--- tests/de/deadlocker8/budgetmaster/tests/server/database/DatabaseHandlerTest.java
+++ tests/de/deadlocker8/budgetmaster/tests/server/database/DatabaseHandlerTest.java
@@ -1,6 +1,7 @@
 package de.deadlocker8.budgetmaster.tests.server.database;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
@@ -9,6 +10,7 @@ import java.net.URISyntaxException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.deadlocker8.budgetmaster.logic.NormalPayment;
 import de.deadlocker8.budgetmasterserver.logic.DatabaseHandler;
 import de.deadlocker8.budgetmasterserver.logic.Settings;
 import de.deadlocker8.budgetmasterserver.logic.Utils;
@@ -45,8 +47,30 @@ public class DatabaseHandlerTest
 	}
 	
 	@Test
-	public void test()
+	public void testNormalPayment()
 	{		
-		assert(true);
+		NormalPayment expectedPayment = new NormalPayment(1, 1000, "2017-03-01", 2, "Buchung", "Lorem Ipsum");	
+		
+		databaseHandler.addNormalPayment(expectedPayment.getAmount(),
+										 expectedPayment.getDate(),
+										 expectedPayment.getCategoryID(),
+										 expectedPayment.getName(),
+										 expectedPayment.getDescription());
+		NormalPayment payment = databaseHandler.getPayment(1);
+		
+		assertEquals(expectedPayment.getAmount(), payment.getAmount());		
+		assertEquals(expectedPayment.getDate(), payment.getDate());
+		assertEquals(expectedPayment.getCategoryID(), payment.getCategoryID());
+		assertEquals(expectedPayment.getName(), payment.getName());
+		assertEquals(expectedPayment.getDescription(), payment.getDescription());
+	}
+	
+	@Test
+	public void testDeleteNormalPayment()
+	{
+		databaseHandler.deletePayment(1);
+		NormalPayment payment = databaseHandler.getPayment(1);
+		
+		assertNull(payment);
 	}
 }
