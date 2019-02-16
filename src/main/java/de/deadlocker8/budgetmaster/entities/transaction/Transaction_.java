b67ad21e6d90b08048d1e595d5ae5676681c838c
Robert Goldmann <deadlocker@gmx.de>
2019-02-16 11:38:07 +0100

    #293 - added missing file

---
+++ src/main/java/de/deadlocker8/budgetmaster/entities/transaction/Transaction_.java
@@ -0,0 +1,25 @@
+package de.deadlocker8.budgetmaster.entities.transaction;
+
+import de.deadlocker8.budgetmaster.entities.Tag;
+import de.deadlocker8.budgetmaster.entities.account.Account;
+import de.deadlocker8.budgetmaster.entities.category.Category;
+import de.deadlocker8.budgetmaster.repeating.RepeatingOption;
+import org.joda.time.DateTime;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import java.util.List;
+
+@StaticMetamodel(Transaction.class)
+public class Transaction_
+{
+	public static volatile SingularAttribute<Transaction, Integer> ID;
+	public static volatile SingularAttribute<Transaction, Integer> amount;
+	public static volatile SingularAttribute<Transaction, DateTime> date;
+	public static volatile SingularAttribute<Transaction, Account> account;
+	public static volatile SingularAttribute<Transaction, Category> category;
+	public static volatile SingularAttribute<Transaction, String> name;
+	public static volatile SingularAttribute<Transaction, String> description;
+	public static volatile SingularAttribute<Transaction, List<Tag>> tags;
+	public static volatile SingularAttribute<Transaction, RepeatingOption> repeatingOption;
+}
