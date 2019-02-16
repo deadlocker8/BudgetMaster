629b4288662d3132e2ae8306892e03b6386fa9f4
Robert Goldmann <deadlocker@gmx.de>
2019-02-16 12:41:24 +0100

    #293 - added TransactionSpecifications + test

--- src/main/java/de/deadlocker8/budgetmaster/entities/transaction/TransactionSpecifications.java
+++ src/main/java/de/deadlocker8/budgetmaster/entities/transaction/TransactionSpecifications.java
@@ -1,5 +1,6 @@
 package de.deadlocker8.budgetmaster.entities.transaction;
 
+import de.deadlocker8.budgetmaster.entities.category.Category;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.Predicate;
@@ -8,21 +9,40 @@ import java.util.List;
 
 public class TransactionSpecifications
 {
-	public static Specification<Transaction> withDynamicQuery(final Integer amount, final String name)
+	public static Specification<Transaction> withDynamicQuery(final boolean isIncome, boolean isExpenditure, final Boolean isRepeating, final List<Category> categories, final Integer amount, final String name)
 	{
 		return (transaction, query, builder) -> {
-			if(amount == null && name == null)
-			{
-				throw new IllegalStateException("At least one parameter should be provided to construct complex query");
-			}
 			List<Predicate> predicates = new ArrayList<>();
-			if(amount != null)
+			if(isIncome && !isExpenditure)
 			{
-				predicates.add(builder.and(builder.equal(transaction.get(Transaction_.amount), amount)));
+				predicates.add(builder.and(builder.gt(transaction.get(Transaction_.amount), 0)));
 			}
+
+			if(!isIncome && isExpenditure)
+			{
+				predicates.add(builder.and(builder.le(transaction.get(Transaction_.amount), 0)));
+			}
+
+			if(isRepeating != null)
+			{
+				if(isRepeating)
+				{
+					predicates.add(builder.and(builder.isNotNull(transaction.get(Transaction_.repeatingOption))));
+				}
+				else
+				{
+					predicates.add(builder.and(builder.isNull(transaction.get(Transaction_.repeatingOption))));
+				}
+			}
+
+			if(categories != null)
+			{
+				predicates.add(builder.and(transaction.get(Transaction_.category).in(categories)));
+			}
+
 			if(name != null)
 			{
-				predicates.add(builder.and(builder.equal(transaction.get(Transaction_.name), name)));
+				predicates.add(builder.and(builder.like(builder.lower(transaction.get(Transaction_.name)), "%"+name.toLowerCase()+"%")));
 			}
 			Predicate[] predicatesArray = new Predicate[predicates.size()];
 			return builder.and(predicates.toArray(predicatesArray));
--- src/test/java/de/deadlocker8/budgetmaster/TransactionRepositoryTest.java
+++ src/test/java/de/deadlocker8/budgetmaster/TransactionRepositoryTest.java
@@ -6,10 +6,15 @@ import de.deadlocker8.budgetmaster.entities.category.Category;
 import de.deadlocker8.budgetmaster.entities.category.CategoryType;
 import de.deadlocker8.budgetmaster.entities.transaction.Transaction;
 import de.deadlocker8.budgetmaster.entities.transaction.TransactionSpecifications;
+import de.deadlocker8.budgetmaster.repeating.RepeatingOption;
+import de.deadlocker8.budgetmaster.repeating.endoption.RepeatingEndAfterXTimes;
+import de.deadlocker8.budgetmaster.repeating.modifier.RepeatingModifierDays;
 import de.deadlocker8.budgetmaster.repositories.AccountRepository;
 import de.deadlocker8.budgetmaster.repositories.CategoryRepository;
+import de.deadlocker8.budgetmaster.repositories.RepeatingOptionRepository;
 import de.deadlocker8.budgetmaster.repositories.TransactionRepository;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +23,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -30,9 +36,11 @@ public class TransactionRepositoryTest
 	private TransactionRepository transactionRepository;
 	private Transaction transaction1;
 	private Transaction transaction2;
+	private Transaction repeatingTransaction;
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	private Category categoryUnused;
 	private Category category1;
 	private Category category2;
 
@@ -40,11 +48,16 @@ public class TransactionRepositoryTest
 	private AccountRepository accountRepository;
 	private Account account;
 
+	@Autowired
+	private RepeatingOptionRepository repeatingOptionRepository;
+	private RepeatingOption repeatingOption;
+
 	@Before
 	public void init()
 	{
 		account = accountRepository.save(new Account("TestAccount", AccountType.CUSTOM));
 
+		categoryUnused = categoryRepository.save(new Category("CategoryUnused", "#00ff00", CategoryType.CUSTOM));
 		category1 = categoryRepository.save(new Category("Category1", "#ff0000", CategoryType.CUSTOM));
 		category2 = categoryRepository.save(new Category("Category2", "#ff0000", CategoryType.CUSTOM));
 
@@ -54,7 +67,7 @@ public class TransactionRepositoryTest
 		transaction1.setDate(new DateTime(2018, 10, 3, 12, 0, 0, 0));
 		transaction1.setCategory(category1);
 		transaction1.setAccount(account);
-		transactionRepository.save(transaction1);
+		transaction1 = transactionRepository.save(transaction1);
 
 		transaction2 = new Transaction();
 		transaction2.setName("Test_2");
@@ -62,36 +75,152 @@ public class TransactionRepositoryTest
 		transaction2.setDate(new DateTime(2018, 10, 3, 12, 0, 0, 0));
 		transaction2.setCategory(category2);
 		transaction2.setAccount(account);
-		transactionRepository.save(transaction2);
+		transaction2 = transactionRepository.save(transaction2);
+
+
+		DateTime repeatingTransactionDate = DateTime.parse("2018-03-13", DateTimeFormat.forPattern("yyyy-MM-dd"));
+		repeatingOption = new RepeatingOption();
+		repeatingOption.setModifier(new RepeatingModifierDays(10));
+		repeatingOption.setStartDate(repeatingTransactionDate);
+		repeatingOption.setEndOption(new RepeatingEndAfterXTimes(2));
+		repeatingOption = repeatingOptionRepository.save(repeatingOption);
+
+		repeatingTransaction = new Transaction();
+		repeatingTransaction.setAmount(-12300);
+		repeatingTransaction.setDate(repeatingTransactionDate);
+		repeatingTransaction.setCategory(category1);
+		repeatingTransaction.setName("Repeating");
+		repeatingTransaction.setDescription("");
+		repeatingTransaction.setAccount(account);
+		repeatingTransaction.setRepeatingOption(repeatingOption);
+		repeatingTransaction.setTags(new ArrayList<>());
+		repeatingTransaction = transactionRepository.save(repeatingTransaction);
 	}
 
 	@Test
-	public void getSpecificTransaction()
+	public void getIncomesAndExpenditures()
 	{
-		Specification spec = TransactionSpecifications.withDynamicQuery(200, "Test");
+		Specification spec = TransactionSpecifications.withDynamicQuery(true, true, null, null, null, null);
+
+		List<Transaction> results = transactionRepository.findAll(spec);
+		assertTrue(results.contains(transaction1));
+		assertTrue(results.contains(transaction2));
+		assertTrue(results.contains(repeatingTransaction));
+	}
+
+	@Test
+	public void getIncomes()
+	{
+		Specification spec = TransactionSpecifications.withDynamicQuery(true, false, null, null, null, null);
 
 		List<Transaction> results = transactionRepository.findAll(spec);
 		assertTrue(results.contains(transaction1));
 		assertFalse(results.contains(transaction2));
+		assertFalse(results.contains(repeatingTransaction));
 	}
 
 	@Test
-	public void oneCriteria()
+	public void getExpenditures()
 	{
-		Specification spec = TransactionSpecifications.withDynamicQuery(-525, null);
+		Specification spec = TransactionSpecifications.withDynamicQuery(false, true, null, null, null, null);
 
 		List<Transaction> results = transactionRepository.findAll(spec);
-		System.out.println(results);
-		assertTrue(results.contains(transaction2));
 		assertFalse(results.contains(transaction1));
+		assertTrue(results.contains(transaction2));
+		assertTrue(results.contains(repeatingTransaction));
 	}
 
 	@Test
-	public void noMatch()
+	public void incomesAndExpendituresFalse()
 	{
-		Specification spec = TransactionSpecifications.withDynamicQuery(855, "eimer");
+		Specification spec = TransactionSpecifications.withDynamicQuery(false, false, null, null, null, null);
 
 		List<Transaction> results = transactionRepository.findAll(spec);
-		assertEquals(0, results.size());
+		assertTrue(results.contains(transaction1));
+		assertTrue(results.contains(transaction2));
+		assertTrue(results.contains(repeatingTransaction));
+	}
+
+	@Test
+	public void getRepeating()
+	{
+		Specification spec = TransactionSpecifications.withDynamicQuery(true, true, true, null, null, null);
+
+		List<Transaction> results = transactionRepository.findAll(spec);
+		assertFalse(results.contains(transaction1));
+		assertFalse(results.contains(transaction2));
+		assertTrue(results.contains(repeatingTransaction));
+	}
+
+	@Test
+	public void noRepeating()
+	{
+		Specification spec = TransactionSpecifications.withDynamicQuery(true, true, false, null, null, null);
+
+		List<Transaction> results = transactionRepository.findAll(spec);
+		assertTrue(results.contains(transaction1));
+		assertTrue(results.contains(transaction2));
+		assertFalse(results.contains(repeatingTransaction));
+	}
+
+	@Test
+	public void noMatchingCategory()
+	{
+		List<Category> categories = new ArrayList<>();
+		categories.add(categoryUnused);
+		Specification spec = TransactionSpecifications.withDynamicQuery(true, true, null, categories, null, null);
+
+		List<Transaction> results = transactionRepository.findAll(spec);
+		assertFalse(results.contains(transaction1));
+		assertFalse(results.contains(transaction2));
+		assertFalse(results.contains(repeatingTransaction));
+	}
+
+	@Test
+	public void getByCategory()
+	{
+		List<Category> categories = new ArrayList<>();
+		categories.add(category1);
+		Specification spec = TransactionSpecifications.withDynamicQuery(true, true, null, categories, null, null);
+
+		List<Transaction> results = transactionRepository.findAll(spec);
+		assertTrue(results.contains(transaction1));
+		assertFalse(results.contains(transaction2));
+		assertTrue(results.contains(repeatingTransaction));
+	}
+
+	@Test
+	public void getByFullName()
+	{
+		Specification spec = TransactionSpecifications.withDynamicQuery(true, true, null, null, null, "Repeating");
+
+		List<Transaction> results = transactionRepository.findAll(spec);
+		assertFalse(results.contains(transaction1));
+		assertFalse(results.contains(transaction2));
+		assertTrue(results.contains(repeatingTransaction));
+	}
+
+	@Test
+	public void getByPartialName()
+	{
+		Specification spec = TransactionSpecifications.withDynamicQuery(true, true, null, null, null, "tin");
+
+		List<Transaction> results = transactionRepository.findAll(spec);
+		assertFalse(results.contains(transaction1));
+		assertFalse(results.contains(transaction2));
+		assertTrue(results.contains(repeatingTransaction));
+	}
+
+	@Test
+	public void getRepeatingExpenditureByCategoryAndName()
+	{
+		List<Category> categories = new ArrayList<>();
+		categories.add(category1);
+		Specification spec = TransactionSpecifications.withDynamicQuery(false, true, true, categories, null, "Repeating");
+
+		List<Transaction> results = transactionRepository.findAll(spec);
+		assertFalse(results.contains(transaction1));
+		assertFalse(results.contains(transaction2));
+		assertTrue(results.contains(repeatingTransaction));
 	}
 }
