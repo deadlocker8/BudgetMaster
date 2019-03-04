7d8790805d1ee9b0fd4d64ad1e97e852d7111114
Robert Goldmann <deadlocker@gmx.de>
2019-03-04 20:56:18 +0100

    Fixed tag meta class

--- src/main/java/de/deadlocker8/budgetmaster/tags/Tag_.java
+++ src/main/java/de/deadlocker8/budgetmaster/tags/Tag_.java
@@ -2,6 +2,7 @@ package de.deadlocker8.budgetmaster.tags;
 
 import de.deadlocker8.budgetmaster.transactions.Transaction;
 
+import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
@@ -9,5 +10,6 @@ import javax.persistence.metamodel.StaticMetamodel;
 public class Tag_
 {
 	public static volatile SingularAttribute<Tag, Integer> ID;
-	public static volatile SingularAttribute<Tag, Integer> amount;
+	public static volatile SingularAttribute<Tag, Integer> name;
+	public static volatile ListAttribute<Tag, Transaction> referringTransactions;
 }
