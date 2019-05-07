03ceb804fcc45c6361d00e0a1aaa3f1868b041c1
tobias <thinkdifferent055@gmail.com>
2019-05-07 22:32:38 +0200

    Removed javafx dependency for category colors

--- src/main/java/de/deadlocker8/budgetmaster/utils/Colors.java
+++ src/main/java/de/deadlocker8/budgetmaster/utils/Colors.java
@@ -1,25 +1,25 @@
 package de.deadlocker8.budgetmaster.utils;
 
-import javafx.scene.paint.Color;
+import de.thecodelabs.utils.util.Color;
 
 public class Colors 
 {
     // CATEGORIES
-    public static final Color CATEGORIES_LIGHT_GREY = Color.web("#EEEEEE");
-    public static final Color CATEGORIES_GREY = Color.web("#888888");
-    public static final Color CATEGORIES_DARK_GREY = Color.web("#333333");
-    public static final Color CATEGORIES_LIGHT_YELLOW = Color.rgb(255,241,119);
-    public static final Color CATEGORIES_YELLOW = Color.rgb(255,204,0);
-    public static final Color CATEGORIES_ORANGE = Color.rgb(255,149,0);
-    public static final Color CATEGORIES_RED = Color.rgb(255,59,48);
-    public static final Color CATEGORIES_DARK_RED = Color.rgb(169,3,41);
-    public static final Color CATEGORIES_PINK = Color.rgb(255,81,151);
-    public static final Color CATEGORIES_PURPLE = Color.rgb(155,89,182);    
-    public static final Color CATEGORIES_DARK_PURPLE = Color.rgb(88,86,214);
-    public static final Color CATEGORIES_BLUE = Color.rgb(0,122,250);
-    public static final Color CATEGORIES_SOFT_BLUE = Color.web("#4F85D6");
-    public static final Color CATEGORIES_LIGHT_BLUE = Color.rgb(90,200,250);
-    public static final Color CATEGORIES_LIGHT_GREEN = Color.rgb(76,217,100);
-    public static final Color CATEGORIES_LIME_GREEN = Color.web("#ABDB64");
-    public static final Color CATEGORIES_DARK_GREEN = Color.rgb(46,124,43);
+    public static final Color CATEGORIES_LIGHT_GREY = new Color("#EEEEEE");
+    public static final Color CATEGORIES_GREY = new Color("#888888");
+    public static final Color CATEGORIES_DARK_GREY = new Color("#333333");
+    public static final Color CATEGORIES_LIGHT_YELLOW = new Color(255,241,119);
+    public static final Color CATEGORIES_YELLOW = new Color(255,204,0);
+    public static final Color CATEGORIES_ORANGE = new Color(255,149,0);
+    public static final Color CATEGORIES_RED = new Color(255,59,48);
+    public static final Color CATEGORIES_DARK_RED = new Color(169,3,41);
+    public static final Color CATEGORIES_PINK = new Color(255,81,151);
+    public static final Color CATEGORIES_PURPLE = new Color(155,89,182);    
+    public static final Color CATEGORIES_DARK_PURPLE = new Color(88,86,214);
+    public static final Color CATEGORIES_BLUE = new Color(0,122,250);
+    public static final Color CATEGORIES_SOFT_BLUE = new Color("#4F85D6");
+    public static final Color CATEGORIES_LIGHT_BLUE = new Color(90,200,250);
+    public static final Color CATEGORIES_LIGHT_GREEN = new Color(76,217,100);
+    public static final Color CATEGORIES_LIME_GREEN = new Color("#ABDB64");
+    public static final Color CATEGORIES_DARK_GREEN = new Color(46,124,43);
 }
