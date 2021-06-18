be23b193cece03ce7ef931ebdbc6da0cd0aa628d
Robert Goldmann <deadlocker@gmx.de>
2021-06-18 23:46:37 +0200

    #622 - backup: added unit tests conversion to internal database

--- src/test/java/de/deadlocker8/budgetmaster/unit/database/DatabaseParser_v7_convertToInternalTest.java
+++ src/test/java/de/deadlocker8/budgetmaster/unit/database/DatabaseParser_v7_convertToInternalTest.java
@@ -6,8 +6,9 @@ import de.deadlocker8.budgetmaster.categories.Category;
 import de.deadlocker8.budgetmaster.categories.CategoryType;
 import de.deadlocker8.budgetmaster.charts.Chart;
 import de.deadlocker8.budgetmaster.charts.ChartType;
-import de.deadlocker8.budgetmaster.database.DatabaseParser_v6;
+import de.deadlocker8.budgetmaster.database.DatabaseParser_v7;
 import de.deadlocker8.budgetmaster.database.InternalDatabase;
+import de.deadlocker8.budgetmaster.icon.Icon;
 import de.deadlocker8.budgetmaster.images.Image;
 import de.deadlocker8.budgetmaster.images.ImageFileExtension;
 import de.deadlocker8.budgetmaster.repeating.RepeatingOption;
@@ -34,7 +35,7 @@ import java.util.Locale;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class DatabaseParser_v6_convertToInternalTest
+public class DatabaseParser_v7_convertToInternalTest
 {
 	@Before
 	public void before()
@@ -61,8 +62,8 @@ public class DatabaseParser_v6_convertToInternalTest
 	{
 		try
 		{
-			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v6Test.json").toURI())));
-			DatabaseParser_v6 importer = new DatabaseParser_v6(json);
+			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v7Test.json").toURI())));
+			DatabaseParser_v7 importer = new DatabaseParser_v7(json);
 			InternalDatabase database = importer.parseDatabaseFromJSON().convertToInternal();
 
 			final Chart chart = new Chart("The best chart", "/* This list will be dynamically filled with all the transactions between\r\n* the start and and date you select on the \"Show Chart\" page\r\n* and filtered according to your specified filter.\r\n* An example entry for this list and tutorial about how to create custom charts ca be found in the BudgetMaster wiki:\r\n* https://github.com/deadlocker8/BudgetMaster/wiki/How-to-create-custom-charts\r\n*/\r\nvar transactionData \u003d [];\r\n\r\n// Prepare your chart settings here (mandatory)\r\nvar plotlyData \u003d [{\r\n    x: [],\r\n    y: [],\r\n    type: \u0027bar\u0027\r\n}];\r\n\r\n// Add your Plotly layout settings here (optional)\r\nvar plotlyLayout \u003d {};\r\n\r\n// Add your Plotly configuration settings here (optional)\r\nvar plotlyConfig \u003d {\r\n    showSendToCloud: false,\r\n    displaylogo: false,\r\n    showLink: false,\r\n    responsive: true\r\n};\r\n\r\n// Don\u0027t touch this line\r\nPlotly.newPlot(\"containerID\", plotlyData, plotlyLayout, plotlyConfig);\r\n", ChartType.CUSTOM, 7);
@@ -82,13 +83,15 @@ public class DatabaseParser_v6_convertToInternalTest
 	{
 		try
 		{
-			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v6Test.json").toURI())));
-			DatabaseParser_v6 importer = new DatabaseParser_v6(json);
+			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v7Test.json").toURI())));
+			DatabaseParser_v7 importer = new DatabaseParser_v7(json);
 			InternalDatabase database = importer.parseDatabaseFromJSON().convertToInternal();
 
-			final Category category = new Category("0815", "#ffcc00", CategoryType.CUSTOM, null);
+			final Icon icon = new Icon("fas fa-icons");
+			icon.setID(2);
+
+			final Category category = new Category("0815", "#ffcc00", CategoryType.CUSTOM, icon);
 			category.setID(3);
-			category.setIcon("fas fa-icons");
 
 			assertThat(database.getCategories()).hasSize(3)
 					.contains(category);
@@ -104,19 +107,24 @@ public class DatabaseParser_v6_convertToInternalTest
 	{
 		try
 		{
-			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v6Test.json").toURI())));
-			DatabaseParser_v6 importer = new DatabaseParser_v6(json);
+			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v7Test.json").toURI())));
+			DatabaseParser_v7 importer = new DatabaseParser_v7(json);
 			InternalDatabase database = importer.parseDatabaseFromJSON().convertToInternal();
 
 			final Image accountImage = new Image(new Byte[0], "awesomeIcon.png", ImageFileExtension.PNG);
 			accountImage.setID(1);
-			final Account account = new Account("Second Account", AccountType.CUSTOM, null);
+
+			final Icon icon = new Icon(accountImage);
+			icon.setID(1);
+
+			final Account account = new Account("Second Account", AccountType.CUSTOM, icon);
 			account.setID(3);
-			account.setIcon(accountImage);
 
 			assertThat(database.getAccounts()).hasSize(3)
 					.contains(account);
-			assertThat(database.getAccounts().get(2).getIcon().getImage())
+			assertThat(database.getAccounts().get(2).getIconReference())
+					.isEqualTo(icon);
+			assertThat(database.getAccounts().get(2).getIconReference().getImage().getImage())
 					.isNotNull()
 					.hasSizeGreaterThan(1);
 		}
@@ -131,8 +139,8 @@ public class DatabaseParser_v6_convertToInternalTest
 	{
 		try
 		{
-			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v6Test.json").toURI())));
-			DatabaseParser_v6 importer = new DatabaseParser_v6(json);
+			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v7Test.json").toURI())));
+			DatabaseParser_v7 importer = new DatabaseParser_v7(json);
 			InternalDatabase database = importer.parseDatabaseFromJSON().convertToInternal();
 
 			final Image image = new Image(new Byte[0], "awesomeIcon.png", ImageFileExtension.PNG);
@@ -155,21 +163,27 @@ public class DatabaseParser_v6_convertToInternalTest
 	{
 		try
 		{
-			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v6Test.json").toURI())));
-			DatabaseParser_v6 importer = new DatabaseParser_v6(json);
+			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v7Test.json").toURI())));
+			DatabaseParser_v7 importer = new DatabaseParser_v7(json);
 			InternalDatabase database = importer.parseDatabaseFromJSON().convertToInternal();
 
 			final Image templateImage = new Image(new Byte[0], "awesomeIcon.png", ImageFileExtension.PNG);
 			templateImage.setID(1);
+
+			final Icon icon = new Icon(templateImage);
+			icon.setID(1);
+
 			final Template template = new Template();
 			template.setTemplateName("Template with icon");
 			template.setIsExpenditure(true);
-			template.setIcon(templateImage);
+			template.setIconReference(icon);
 			template.setTags(List.of());
 
 			assertThat(database.getTemplates()).hasSize(4)
 					.contains(template);
-			assertThat(database.getTemplates().get(3).getIcon().getImage())
+			assertThat(database.getTemplates().get(3).getIconReference())
+					.isEqualTo(icon);
+			assertThat(database.getTemplates().get(3).getIconReference().getImage().getImage())
 					.isNotNull()
 					.hasSizeGreaterThan(1);
 		}
@@ -184,8 +198,8 @@ public class DatabaseParser_v6_convertToInternalTest
 	{
 		try
 		{
-			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v6Test.json").toURI())));
-			DatabaseParser_v6 importer = new DatabaseParser_v6(json);
+			String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v7Test.json").toURI())));
+			DatabaseParser_v7 importer = new DatabaseParser_v7(json);
 			InternalDatabase database = importer.parseDatabaseFromJSON().convertToInternal();
 
 			Account account1 = new Account("Default", AccountType.CUSTOM);
@@ -194,15 +208,21 @@ public class DatabaseParser_v6_convertToInternalTest
 			Image image = new Image(new Byte[0], "awesomeIcon.png", ImageFileExtension.PNG);
 			image.setID(1);
 
+			Icon accountIcon = new Icon(image);
+			accountIcon.setID(1);
+
 			Account account2 = new Account("Second Account", AccountType.CUSTOM);
-			account2.setIcon(image);
+			account2.setIconReference(accountIcon);
 			account2.setID(3);
 
 			Category categoryNone = new Category("Keine Kategorie", "#FFFFFF", CategoryType.NONE);
 			categoryNone.setID(1);
 
+			Icon categoryIcon = new Icon("fas fa-icons");
+			categoryIcon.setID(2);
+
 			Category category3 = new Category("0815", "#ffcc00", CategoryType.CUSTOM);
-			category3.setIcon("fas fa-icons");
+			category3.setIconReference(categoryIcon);
 			category3.setID(3);
 
 			Transaction normalTransaction_1 = new Transaction();
@@ -250,7 +270,7 @@ public class DatabaseParser_v6_convertToInternalTest
 			repeatingTransaction_1.setTags(new ArrayList<>());
 			repeatingTransaction_1.setIsExpenditure(true);
 
-		    Transaction transferTransaction = new Transaction();
+			Transaction transferTransaction = new Transaction();
 			transferTransaction.setAmount(-250);
 			DateTime transferTransactionDate = DateTime.parse("2018-06-15", DateTimeFormat.forPattern("yyyy-MM-dd"));
 			transferTransactionDate = transferTransactionDate.withHourOfDay(12).withMinuteOfHour(0).withSecondOfMinute(0);
@@ -274,5 +294,28 @@ public class DatabaseParser_v6_convertToInternalTest
 		{
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void test_Icons() throws IOException, URISyntaxException
+	{
+		String json = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("DatabaseParser_v7Test.json").toURI())));
+		DatabaseParser_v7 importer = new DatabaseParser_v7(json);
+		InternalDatabase database = importer.parseDatabaseFromJSON().convertToInternal();
+
+		final Image image = new Image(new Byte[0], "awesomeIcon.png", ImageFileExtension.PNG);
+		image.setID(1);
+
+		final Icon icon1 = new Icon(image);
+		icon1.setID(1);
+
+		final Icon icon2 = new Icon("fas fa-icons");
+		icon2.setID(2);
+
+		assertThat(database.getIcons()).hasSize(2)
+				.containsExactly(icon1, icon2);
+		assertThat(database.getImages().get(0).getImage())
+				.isNotNull()
+				.hasSizeGreaterThan(1);
 	}
 }
