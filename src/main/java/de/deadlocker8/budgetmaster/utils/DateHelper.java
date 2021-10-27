e83bab8304932fb21d21c3a1701a25028af23766
Robert Goldmann <deadlocker@gmx.de>
2021-10-27 23:07:34 +0200

    #646 - fixed wrong transaction date after midnight in timezone Berlin

---
+++ src/main/java/de/deadlocker8/budgetmaster/utils/DateHelper.java
@@ -0,0 +1,14 @@
+package de.deadlocker8.budgetmaster.utils;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+import java.util.TimeZone;
+
+public class DateHelper
+{
+	public static DateTime getCurrentDate()
+	{
+		return DateTime.now(DateTimeZone.forTimeZone(TimeZone.getDefault()));
+	}
+}
