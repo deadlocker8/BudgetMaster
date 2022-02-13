441350b6dda787039adcfbd61487a31a2425abbe
Robert Goldmann <deadlocker@gmx.de>
2022-02-13 16:37:26 +0100

    #672: fixed selenium tests

--- src/test/java/de/deadlocker8/budgetmaster/integration/selenium/ChartTest.java
+++ src/test/java/de/deadlocker8/budgetmaster/integration/selenium/ChartTest.java
@@ -175,7 +175,7 @@ class ChartTest extends SeleniumTestBase
 		assertThat(displayedChartPreviews)
 				.hasSize(3);
 		assertThat(displayedChartPreviews.get(0).findElement(By.cssSelector(".card-action span")).getText())
-				.isEqualTo("Average incomes/expenditures");
+				.isEqualTo("Average monthly incomes/expenditures");
 
 		// filter
 		assertThat(driver.findElement(By.id("filterActiveBadge")).isDisplayed()).isFalse();
