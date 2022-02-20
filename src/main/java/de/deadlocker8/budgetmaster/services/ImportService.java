529f9afc9111e87fa9f98d70242b8ffef908510f
Robert Goldmann <deadlocker@gmx.de>
2022-02-20 18:07:06 +0100

    cleanup

--- src/main/java/de/deadlocker8/budgetmaster/services/ImportService.java
+++ src/main/java/de/deadlocker8/budgetmaster/services/ImportService.java
@@ -33,7 +33,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
