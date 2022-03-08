881015ca334c6e35ad7d91fc6f59299e6eb9a51a
Robert Goldmann <deadlocker@gmx.de>
2022-03-08 22:29:05 +0100

    #663 - migrator: removed unnecessary JpaRepositoryies

--- BudgetMasterDatabaseMigrator/src/main/java/de/deadlocker8/budgetmaster/databasemigrator/source/category/SourceCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package de.deadlocker8.budgetmaster.databasemigrator.source.category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SourceCategoryRepository extends JpaRepository<SourceCategory, Integer>
-{
-}
--- BudgetMasterDatabaseMigrator/src/main/java/de/deadlocker8/budgetmaster/databasemigrator/source/image/SourceImageRepository.java
+++
@@ -1,7 +0,0 @@
-package de.deadlocker8.budgetmaster.databasemigrator.source.image;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SourceImageRepository extends JpaRepository<SourceImage, Integer>
-{
-}
