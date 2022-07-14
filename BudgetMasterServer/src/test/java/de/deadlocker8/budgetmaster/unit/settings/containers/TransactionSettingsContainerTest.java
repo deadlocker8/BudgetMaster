8bb0e84f33ea3fa81c3ea8965149b57a5aa7be0f
Robert Goldmann <deadlocker@gmx.de>
2022-07-14 19:58:53 +0200

    #696 - added tests for TransactionSettingsContainer

---
+++ BudgetMasterServer/src/test/java/de/deadlocker8/budgetmaster/unit/settings/containers/TransactionSettingsContainerTest.java
@@ -0,0 +1,62 @@
+package de.deadlocker8.budgetmaster.unit.settings.containers;
+
+import de.deadlocker8.budgetmaster.settings.Settings;
+import de.deadlocker8.budgetmaster.settings.SettingsService;
+import de.deadlocker8.budgetmaster.settings.containers.TransactionsSettingsContainer;
+import de.deadlocker8.budgetmaster.unit.helpers.LocalizedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(SpringExtension.class)
+@LocalizedTest
+class TransactionSettingsContainerTest
+{
+	@Mock
+	private SettingsService settingsService;
+
+	@Test
+	void test_validate_valid()
+	{
+		final TransactionsSettingsContainer container = new TransactionsSettingsContainer(true);
+
+		final Errors errors = new BeanPropertyBindingResult(container, "container");
+		container.validate(errors);
+
+		assertThat(errors.getAllErrors())
+				.isEmpty();
+	}
+
+	@Test
+	void test_fixBooleans()
+	{
+		final TransactionsSettingsContainer container = new TransactionsSettingsContainer(null);
+
+		container.fixBooleans();
+
+		assertThat(container)
+				.hasFieldOrPropertyWithValue("restActivated", false);
+	}
+
+
+	@Test
+	void test_updateSettings()
+	{
+		final Settings defaultSettings = Settings.getDefault();
+
+		Mockito.when(settingsService.getSettings()).thenReturn(defaultSettings);
+
+		final TransactionsSettingsContainer container = new TransactionsSettingsContainer(false);
+		final Settings updatedSettings = container.updateSettings(settingsService);
+
+		assertThat(updatedSettings)
+				.hasFieldOrPropertyWithValue("restActivated", false);
+	}
+}
