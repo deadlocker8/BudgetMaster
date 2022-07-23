3f3befa6db8869b6fec07baacaef0e710bdddc95
Robert Goldmann <deadlocker@gmx.de>
2022-07-23 19:08:32 +0200

    #677 - cleanup

--- BudgetMasterServer/src/main/java/de/deadlocker8/budgetmaster/transactions/keywords/TransactionNameKeywordService.java
+++ BudgetMasterServer/src/main/java/de/deadlocker8/budgetmaster/transactions/keywords/TransactionNameKeywordService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Locale;
 
 @Service
 public class TransactionNameKeywordService implements Resettable
